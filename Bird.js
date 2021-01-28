4e4c59032706191261831fb8e9c38f195341a05d
Joycee Singh <apple@Joycees-MacBook-Air.local>
2021-01-28 11:42:12 +0530

    1st commit

---
+++ Bird.js
@@ -0,0 +1,29 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+    if(this.body.speed > 14 && this.body.position.x > 220)
+    {
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+   // [[x1,y1] , [x2,y2] , [x3,y3],.........]
+    //if(gameState === "launched")
+    console.log(this.trajectory)
+    for(var i = 0; i < this.trajectory.length; i++)
+    {
+     image(this.smokeImage, this.trajectory[i][0],this.trajectory[i][1] )  
+    }
+
+
+  }
+}
